710a79580af525a0671288eb38a1923fed74aea1
arrinal <arrinalsf@gmail.com>
2025-05-06 23:05:50 +0700

    Initial Commit

---
+++ PeeLog/PeeLogApp.swift
@@ -0,0 +1,32 @@
+//
+//  PeeLogApp.swift
+//  PeeLog
+//
+//  Created by Arrinal S on 06/05/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PeeLogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PeeLogTests/PeeLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  PeeLogTests.swift
+//  PeeLogTests
+//
+//  Created by Arrinal S on 06/05/25.
+//
+
+import Testing
+@testable import PeeLog
+
+struct PeeLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
